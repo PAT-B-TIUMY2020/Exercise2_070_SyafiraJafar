dceb1c2971038394cd0618ba18d2bfed69f94078
syafiraJafar <56532146+syafiraJafar@users.noreply.github.com>
2020-12-25 23:51:59 +0700

    Exercise 2 WCF Rest

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbNIM = new System.Windows.Forms.TextBox();
+            this.tbProdi = new System.Windows.Forms.TextBox();
+            this.tbAngkatan = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NIM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Prodi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Angkatan";
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(138, 89);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(377, 22);
+            this.tbSearch.TabIndex = 5;
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(138, 130);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(377, 22);
+            this.tbNama.TabIndex = 6;
+            // 
+            // tbNIM
+            // 
+            this.tbNIM.Location = new System.Drawing.Point(138, 179);
+            this.tbNIM.Name = "tbNIM";
+            this.tbNIM.Size = new System.Drawing.Size(377, 22);
+            this.tbNIM.TabIndex = 7;
+            // 
+            // tbProdi
+            // 
+            this.tbProdi.Location = new System.Drawing.Point(138, 226);
+            this.tbProdi.Name = "tbProdi";
+            this.tbProdi.Size = new System.Drawing.Size(377, 22);
+            this.tbProdi.TabIndex = 8;
+            // 
+            // tbAngkatan
+            // 
+            this.tbAngkatan.Location = new System.Drawing.Point(138, 267);
+            this.tbAngkatan.Name = "tbAngkatan";
+            this.tbAngkatan.Size = new System.Drawing.Size(377, 22);
+            this.tbAngkatan.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(578, 135);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(578, 187);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 12;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(578, 229);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 13;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(578, 270);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 14;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 340);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(620, 165);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(817, 549);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbAngkatan);
+            this.Controls.Add(this.tbProdi);
+            this.Controls.Add(this.tbNIM);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbNIM;
+        private System.Windows.Forms.TextBox tbProdi;
+        private System.Windows.Forms.TextBox tbAngkatan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
---
+++ ServerExercise/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceLibraryExercise;
+
+namespace ServerExercise
+{
+    public partial class Form1 : Form
+    {
+        ServiceHost hostobjek = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnOn.Enabled = true;
+            btnOff.Enabled = false;
+            txtServer.Enabled = false;
+            txtServer.Text = "Server Off!!";           
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            btnOn.Enabled = false;
+            btnOff.Enabled = true;
+            hostobjek = new ServiceHost(typeof(Service1));
+            hostobjek.Open();
+            txtServer.Text = "Server ON!!";
+            hostobjek.Close();
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            btnOn.Enabled = true;
+            btnOff.Enabled = false;
+            hostobjek = null;
+            txtServer.Text = "Server Off!!";
+        }
+    }
+}
